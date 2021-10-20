5939939d49edacc4c6df4f887a1085cf2c5d2411
Tyler Mayoff <tyler@tylermayoff.com>
2021-10-19 21:49:02 -0400

    Changed the default log file name

--- Develle/src/Develle/Core/Log.cpp
+++ Develle/src/Develle/Core/Log.cpp
@@ -13,7 +13,7 @@ void Log::Init() {
   logSinks.emplace_back(
       std::make_shared<spdlog::sinks::stdout_color_sink_mt>());
   logSinks.emplace_back(
-      std::make_shared<spdlog::sinks::basic_file_sink_mt>("Develle_Log", true));
+      std::make_shared<spdlog::sinks::basic_file_sink_mt>("Develle.log", true));
 
   logSinks[0]->set_pattern("%^[%T] %n: %v%$");
   logSinks[1]->set_pattern("[%T] [%l] %n: %v");
