1614a1796443d5ad54a4fcf0dbc54bff5a025693
Tyler Mayoff <tyler@tylermayoff.com>
2022-02-13 14:23:56 -0500

    Fixed Camera class

--- Develle/src/Develle/Renderer/Camera.hpp
+++ Develle/src/Develle/Renderer/Camera.hpp
@@ -2,21 +2,35 @@
 #define CAMERA_HPP_
 
 #include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
 
 namespace Develle {
 
 class Camera {
-public:
+ public:
   Camera() = default;
+  Camera(Camera &&cam) : projection(cam.projection) {}
+  Camera(const Camera &cam) : projection(cam.projection) {}
   Camera(const glm::mat4 &projection) : projection(projection) {}
+  Camera &operator=(const Camera &) = default;
+  Camera &operator=(Camera &&) = default;
 
   virtual ~Camera() = default;
 
-  const glm::mat4 &GetProjection() const { return projection; }
+  void SetPosition(const glm::vec3 &pos) { position = pos; }
 
-protected:
-  glm::mat4 projection = glm::mat4(1.0f);
+  const glm::mat4 &GetProjection() const { return projection; }
+  void SetProjection(const glm::mat4 &mat) { projection = mat; }
+
+  glm::mat4 GetViewProjection() const {
+    DV_PROFILE_FUNCTION();  // NOLINT
+    return projection * glm::inverse(glm::translate(glm::mat4(1.0), position));
+  }
+
+ private:
+  glm::mat4 projection = glm::mat4(1.0f);  // NOLINT
+  glm::vec3 position = glm::vec3(0.0f);
 };
 
-} // namespace Develle
-#endif // CAMERA_HPP_
+}  // namespace Develle
+#endif  // CAMERA_HPP_
