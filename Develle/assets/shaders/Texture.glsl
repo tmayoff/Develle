f1409c9b2e2a75860e36c297ebc6dd85f14cfb6c
Tyler Mayoff <tyler@tylermayoff.com>
2022-02-13 17:44:17 -0500

    changed shader

--- Develle/assets/shaders/Texture.glsl
+++ Develle/assets/shaders/Texture.glsl
@@ -13,18 +13,18 @@ layout(std140, binding = 0) uniform Camera { mat4 u_ViewProjection; };
 struct VertexOutput {
   vec4 Color;
   vec2 TexCoord;
-  float TexIndex;
   float TilingFactor;
 };
 
 layout(location = 0) out VertexOutput Output;
+layout(location = 3) out flat float v_TexIndex;
 layout(location = 4) out flat int v_EntityID;
 
 void main() {
   Output.Color = a_Color;
   Output.TexCoord = a_TexCoord;
-  Output.TexIndex = a_TexIndex;
   Output.TilingFactor = a_TilingFactor;
+  v_TexIndex = a_TexIndex;
   v_EntityID = a_EntityID;
 
   // Set vertex position
@@ -40,18 +40,18 @@ layout(location = 1) out int entityID;
 struct VertexOutput {
   vec4 Color;
   vec2 TexCoord;
-  float TexIndex;
   float TilingFactor;
 };
 
 layout(location = 0) in VertexOutput Input;
+layout(location = 3) in flat float v_TexIndex;
 layout(location = 4) in flat int v_EntityID;
 
 layout(binding = 0) uniform sampler2D u_Textures[32];
 
 void main() {
   vec4 texColor = Input.Color;
-  switch (int(Input.TexIndex)) {
+  switch (int(v_TexIndex)) {
   case 0:
     texColor *= texture(u_Textures[0], Input.TexCoord * Input.TilingFactor);
     break;
